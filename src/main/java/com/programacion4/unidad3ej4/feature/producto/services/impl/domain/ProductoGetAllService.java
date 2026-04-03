98c24b70b84f52b6d4ecb2b2849b0009464608c7
RamiroHH <ramiroh.dev@gmail.com>
2026-04-03 10:18:52 -0300

    Create ProductoGetAllService.java

---
+++ src/main/java/com/programacion4/unidad3ej4/feature/producto/services/impl/domain/ProductoGetAllService.java
@@ -0,0 +1,26 @@
+package com.programacion4.unidad3ej4.feature.producto.services.impl.domain;
+
+import com.programacion4.unidad3ej4.feature.producto.dtos.response.ProductoResponseDto;
+import com.programacion4.unidad3ej4.feature.producto.mappers.ProductoMapper;
+import com.programacion4.unidad3ej4.feature.producto.repositories.IProductoRepository;
+import com.programacion4.unidad3ej4.feature.producto.services.interfaces.domain.IProductoGetAllService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProductoGetAllService implements IProductoGetAllService {
+
+    private final IProductoRepository productoRepository;
+
+    @Override
+    public List<ProductoResponseDto> getAll() {
+
+        return productoRepository.findByEstaEliminadoFalse()
+                .stream()
+                .map(ProductoMapper::toResponseDto)
+                .toList();
+    }
+}
